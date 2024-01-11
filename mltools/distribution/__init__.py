c7054d354e2a46527e206d39bef642bd6ea27acb
szmidapatrik <patriksz25@gmail.com>
2024-01-11 20:11:39 +0100

    PDF function shape estimator done

--- mltools/distribution/__init__.py
+++ mltools/distribution/__init__.py
@@ -2,3 +2,4 @@ from .normal import NormalDistribution
 from .laplace import LaplaceDistribution
 from .studentsτ import StudentsτDistribution
 from .exponential import ExponentialDistribution
+from .estimate_pdf import EstimatePDF
